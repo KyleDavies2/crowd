513fd0e92e717c391349d0adf8131496562379c0
DaviesDigital <info@daviesdigital.ca>
2023-12-09 19:01:51 -0700

    sd

---
+++ src/routes/gig-payments/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import 'iconify-icon'
+</script>
+<div class="grid grid-cols-10">
+    <div class="col-span-1 bg-white p-5 h-[100vh] border border-r-1">
+
+        <iconify-icon icon="carbon:generate-pdf" width="1.2rem" class="mr-2"></iconify-icon>
+    </div>
+    <div class="p-5 bg-gray-50 col col-span-9 p-4">
+        <div class="grid grid-cols-2">
+            <div class="col-span-1 p-4">
+                <div class="flex justify-between">
+                    <div class="flex">
+                        <iconify-icon icon="carbon:arrow-left" width="1.2rem" class="mr-2"></iconify-icon>
+                        <p class="text-sm">Back</p>
+                    </div>
+                    <div class="flex w-16 justify-between">
+
+                        <iconify-icon icon="carbon:filter" width="1.2rem" class="mr-2"></iconify-icon>
+                        <iconify-icon icon="carbon:notification-new" width="1.2rem" class="mr-2"></iconify-icon>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-span-1 bg-green-950 bg rounded-tl-3xl rounded-tr-3xl min-h-screen text-white  p-4">
+
+            </div>
+        </div>
+        <div class="bg-blu">
+
+        </div>
+    </div>
+</div>
+
